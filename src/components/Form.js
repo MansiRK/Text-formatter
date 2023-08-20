75949a4aad109412714f0deff3a316f5e54f0f08
MansiRK <19302009mansi@viva-technology.org>
2023-08-20 14:00:23 +0530

    Added convert to uppercase

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,31 @@
-
 import "./Form.css"
+import React, {useState} from "react"
 
-function Form(props) {
+
+function Form(props){
+    // Array destructuring syntax
+    // text = "new text"; Wrong way to change the state
+    // setText("new text"); Correct way to change the state
+    
+    const handleUpperCase = () => {
+        console.log("You clicked uppercase button" + text) 
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+
+    const handleOnChange = (event) => {
+        console.log("You click handle on change")
+        // Updating text
+        setText(event.target.value)
+    }
+
+  const [text, setText] = useState("Enter text here");
+
   return (
-    <div className='form mb-3'>
-        <h3>{props.heading}</h3>
-      <label htmlFor="myBox" className='form-label'>Enter text here</label>
-      <textarea name="textBox" className="form-control" cols="60" rows="10"></textarea>
+    <div className="form mb-3">
+      <h3>{props.heading} </h3>
+      <textarea className="form-control" name="textBox" value={text} onChange={handleOnChange} cols="60" rows="10"></textarea>
+      <button className="btn btn-dark" onClick={handleUpperCase}>Convert to Uppercase</button>
     </div>
   )
 }
