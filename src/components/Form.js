5bf7312051f38ba034f8199c98dc873a780f2bc2
MansiRK <19302009mansi@viva-technology.org>
2023-08-23 00:17:32 +0530

    added functionalities

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,26 +7,75 @@ function Form(props){
     // text = "new text"; Wrong way to change the state
     // setText("new text"); Correct way to change the state
     
+    const [text, setText] = useState(" ");
+
     const handleUpperCase = () => {
-        console.log("You clicked uppercase button" + text) 
+        // console.log("You clicked uppercase button" + text) 
         let newText = text.toUpperCase()
         setText(newText)
     }
 
+    const handleLowerCase = () => {
+      let newText = text.toLowerCase()
+      setText(newText)
+    }
+
+    const handleRemoveSpaces = () => {
+      let newText = text.trim()
+      setText(newText)
+    }
+
+    const handleCopy = () => {
+      navigator.clipboard.writeText(text)
+      alert("You copied the text")
+    }
+
+    const handleClear = () => {
+      let clearText = ""
+      setText(clearText)
+      alert("You cleared the text")
+    }
+
     const handleOnChange = (event) => {
-        console.log("You click handle on change")
+        // console.log("You click handle on change")
         // Updating text
         setText(event.target.value)
     }
 
-  const [text, setText] = useState("Enter text here");
+  
 
   return (
-    <div className="form mb-3">
+    <>
+    <div className="container my-5">
       <h3>{props.heading} </h3>
-      <textarea className="form-control" name="textBox" value={text} onChange={handleOnChange} cols="60" rows="10"></textarea>
-      <button className="btn btn-dark" onClick={handleUpperCase}>Convert to Uppercase</button>
+
+      <div className="my-3">
+      <textarea className="form-control" name="textBox" value={text} onChange={handleOnChange} placeholder="Enter text here to modify" cols="60" rows="10"></textarea>
+      </div>
+
+      <button className="btn btn-dark m-3" onClick={handleUpperCase}>Convert to Uppercase</button>
+
+      <button className="btn btn-dark m-3" onClick={handleLowerCase}>Convert to Lowercase</button>
+
+      <button className="btn btn-dark m-3" onClick={handleRemoveSpaces}>Remove spaces</button>
+
+      <button className="btn btn-dark m-3" onClick={handleCopy}>Copy text</button>
+
+      <button className="btn btn-danger m-3" onClick={handleClear}>Clear text</button>
+
     </div>
+
+    <div className="container">
+
+    <h3>Your text summary</h3>
+
+    <p>Your text has <b>{text.split(" ").length - 1}</b> words and <b>{text.length}</b> characters</p>
+    <p><b>{0.008 * text.split(" ").length}</b> Minutes read time</p>
+
+    <h3>Preview</h3>
+    <p>{text}</p>
+    </div>
+     </>
   )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,29 @@ import PropTypes from 'prop-types'
 
 //Passing props
 function Nav(props) {
+
   return (
-    <div className='container'>
-      <nav className='navbar'>
-        <h2>Text-Formatter</h2>
-        <ul className='nav-links'>
-            <li>{props.home}</li>
-            <li>{props.about}</li>
-            <li>Contact</li>
-        </ul>
-      </nav>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Text-Formatter</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-2 mb-2 mb-lg-0 align-items-end">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">{props.about}</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Contact</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   )
 }
 
@@ -29,7 +41,7 @@ Nav.propTypes =
 
 // If prop is not passed then this are the default props
 Nav.defaultProps = {
-  title: "Set title here",
+  home: "Set Home title here",
   about: "Set about text here"
 }
 
